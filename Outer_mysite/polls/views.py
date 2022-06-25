75372c55e760f7afd5d3e01a270d2097de148b56
SachitSankhe <sachitsankhe@gmail.com>
2022-06-25 13:28:35 +0530

    Part-3 completed(Learnt more about views,how to create and use custome templates,how to handle errors and also how to use namespacing in url)

--- Outer_mysite/polls/views.py
+++ Outer_mysite/polls/views.py
@@ -1,7 +1,43 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from multiprocessing import context
+from django.template import loader
+from django.shortcuts import get_object_or_404, render
+from django.http import Http404, HttpResponse
+
+from .models import Question
 # Create your views here.
 
 
 def index(request):
-    return HttpResponse("Hello,World. You're at the polls index.")
+    latest_questions_list = Question.objects.order_by('pub_date')[:5]
+    #To use inbuilt template
+    # output = ", ".join([q.question_text for q in latest_questions_list])
+    # return HttpResponse(output)
+    template = loader.get_template('polls/index.html')
+    context = {
+        'latest_question_list': latest_questions_list,
+    }
+    return HttpResponse(template.render(context, request))
+    #Shortcut
+    # context = {
+    #     'latest_question_list': latest_questions_list,
+    # }
+    # return render(request, 'polls/index.html', context)
+
+
+def detail(request, question_id):
+    # try:
+    #     question = Question.objects.get(pk=question_id)
+    # except:
+    #     raise Http404("Question does not exist")
+    # return render(request,"polls/details.html",{'question':question})
+    #Shortcut
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/details.html", {'question': question})
+
+
+def results(request, question_id):
+    response = "You're looking at results of question %s."
+    return HttpResponse(response % question_id)
+
+def votes(request,question_id):
+    return HttpResponse("You're voting on question %s." %question_id)
